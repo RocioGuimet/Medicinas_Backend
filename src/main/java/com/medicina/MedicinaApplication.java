318fb7267f1ad4fd32cd96b9025d7b100ab7a65f
unknown <rocioguimet@hotmail.com>
2026-02-11 18:53:28 -0300

    Creo WebConfig para resolver conflictos de mapping de Swagger UI con static frontend

--- src/main/java/com/medicina/MedicinaApplication.java
+++ src/main/java/com/medicina/MedicinaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    MedicinaApplication {
+public class MedicinaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedicinaApplication.class, args);
